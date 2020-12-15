1127a16b5497ce278beddf6f71b39b51583e9fea
rahuljha661 <rahuljha12527@gmail.com>
2020-12-15 10:51:30 +0530

    add middleware

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan=require('morgan');
+const colors=require('colors');
+const connectDB=require('./config/db');
 
-//Route files
-const bootcamps=require('./routes/bootcamps');
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
+
+
 const app = express();
 
+
+//Dev logging middleware
+if(process.env.NODE_ENV==='development'){
+   app.use(morgan('dev'));
+}
 //Mount routers
-app.use('/api/v1/bootcamps',bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server runnig in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server=app.listen(PORT, () => {
+  console.log(`Server runnig in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
+
+//Handle unhandled rejection
+process.on('unhandeledRejection',(err,promise)=>{
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit process
+  server.close(()=>{
+    process.exit(1)
+  })
+});
+
+
