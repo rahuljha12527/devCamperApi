60066bc048e07bf7af9e2aa95833ac238cc7c59b
rahuljha661 <rahuljha12527@gmail.com>
2020-12-17 16:16:53 +0530

    error Handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const errorHandler=require('./middleware/error');
 const connectDB = require("./config/db");
 var bodyParser = require("body-parser").json();
 const cookieParser = require("cookie-parser");
@@ -32,12 +33,9 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Mount routers
-app.use("/api/v1/bootcamps", bootcamps); //routes.
+app.use("/api/v1/bootcamps", bootcamps); 
 
-// `app.post('/create', function(req, res) {
-//   console.low`g(">>>>>>>>>>>>>>",req);
-//   res.status(200).se`wa21nd("Doneeeeeeeeeeeeeeeeeee");
-// });`
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
